a618e520078b78ef512a1dc223bd68b6f9d28635
zsx12138 <2784906330@qq.com>
2024-07-06 18:51:02 +0800

    chore: :twisted_rightwards_arrows: 解决合并冲突

--- ruoyi/ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi/ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 启动程序
- * 
+ *
  * @author ruoyi
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
@@ -28,4 +28,3 @@ public class RuoYiApplication
                 " ''-'   `'-'    `-..-'              ");
     }
 }
-//123456
