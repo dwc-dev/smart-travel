890d37f86cd653805ea5575160a3c1e4a29655fc
zsx12138 <2784906330@qq.com>
2024-07-06 22:43:58 +0800

    feat(sql): :sparkles: travel_scenic表添加image_width、image_height、image_main_color

--- sql/travel.sql
+++ sql/travel.sql
@@ -5,7 +5,7 @@ CREATE TABLE travel_user (
     password VARCHAR(255) NOT NULL COMMENT '密码',
     role ENUM('售票管理员', '内容管理员', '普通用户') NOT NULL COMMENT '用户角色',
     create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间'
-) COMMENT='用户表，存储所有类型用户的信息';
+) COMMENT = '用户表，存储所有类型用户的信息';
 
 -- 景区表：存储景区信息
 CREATE TABLE travel_scenic (
@@ -16,8 +16,11 @@ CREATE TABLE travel_scenic (
     district VARCHAR(50) NOT NULL COMMENT '区',
     description TEXT COMMENT '景区简介',
     image_url VARCHAR(255) COMMENT '景区图片URL',
-    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间'
-) COMMENT='景区表，存储景区信息';
+    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    image_width INT COMMENT '景区图片宽度',
+    image_height INT COMMENT '景区图片高度',
+    image_main_color VARCHAR(50) COMMENT '景区图片主色'
+) COMMENT = '景区表，存储景区信息';
 
 -- 票务表：存储票的数量和价格等信息
 CREATE TABLE travel_ticket (
@@ -27,7 +30,7 @@ CREATE TABLE travel_ticket (
     type INT NOT NULL COMMENT '票的类型',
     price DECIMAL(10, 2) NOT NULL COMMENT '票的价格',
     create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间'
-) COMMENT='票务表，存储票的数量和价格等信息';
+) COMMENT = '票务表，存储票的数量和价格等信息';
 
 -- 订单表：存储用户的购票信息
 CREATE TABLE travel_order (
@@ -37,4 +40,4 @@ CREATE TABLE travel_order (
     quantity INT NOT NULL COMMENT '购票数量',
     total_price DECIMAL(10, 2) NOT NULL COMMENT '总价',
     order_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '下单时间'
-) COMMENT='订单表，存储用户的购票信息';
+) COMMENT = '订单表，存储用户的购票信息';
