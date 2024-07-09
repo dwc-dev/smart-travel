fc6d0c908711776dbb2c68178812faaccc8dbd20
zsx12138 <2784906330@qq.com>
2024-07-09 13:51:13 +0800

    feat(vue): :sparkles: Detail content

--- vue/src/components/FsBookDetail.vue
+++ vue/src/components/FsBookDetail.vue
@@ -12,10 +12,23 @@ const props = defineProps<{
   <Transition>
     <div v-if="props.show" class="fs-book-detail">
       <div class="image">
-        <img v-if="data?.imageUrl" :src="data.imageUrl" :alt="data.name" />
+        <img
+          v-if="data?.imageUrl"
+          class="w-full"
+          :src="data.imageUrl"
+          :alt="data.name"
+        />
       </div>
       <div class="content">
-        {{ data }}
+        <template v-if="data?.name">
+          <h1>{{ data.name }}</h1>
+        </template>
+        <template v-if="data?.address">
+          <h2>{{ data.address }}</h2>
+        </template>
+        <template v-if="data?.description">
+          <p>{{ data.description }}</p>
+        </template>
       </div>
     </div>
   </Transition>
@@ -45,16 +58,39 @@ const props = defineProps<{
     flex: 1;
     width: 0;
     padding: 1rem;
+    overflow-y: scroll;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+
+    h1 {
+      margin-top: 20px;
+      color: deeppink;
+      text-align: center;
+      font-size: 22px;
+      font-weight: bold;
+      text-wrap: nowrap;
+    }
+
+    h2 {
+      margin-top: 10px;
+      margin-bottom: 10px;
+      font-family: '华文行楷', 'Microsoft YaHei UI', sans-serif;
+      text-align: right;
+      font-size: 16px;
+      text-wrap: nowrap;
+    }
+
+    p {
+      text-indent: 2em;
+    }
   }
 }
 
 .v-enter-active,
 .v-leave-active {
-  transition:
-    transform 0.4s ease 0s,
-    height 0.4s ease 0s,
-    width 0.4s ease 0s,
-    opacity 0.4s ease 0s;
+  transition: all 0.4s ease 0s;
 }
 
 .v-enter-from,
